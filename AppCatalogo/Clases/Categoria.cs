b806faeb7b99cd08b324b2108c615dc7da592fe8
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-07-17 02:58:11 -0300

    Se agregaron las clases Articulo, Categoria y Marca para las entidades del catálogo.

---
+++ AppCatalogo/Clases/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCatalogo.Clases
+{
+    internal class Categoria
+    {
+        public int Id { get; set; }
+
+        // Nombre de la categoría (ej: Smartphones, Laptops)
+        public string Descripcion { get; set; } = string.Empty; // Inicialización para evitar valores nulos
+    }
+}
